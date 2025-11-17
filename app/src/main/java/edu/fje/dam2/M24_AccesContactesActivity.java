b7fe673aedede6ed9a7d207cf6236f544b45fe3d
sergi grau <sergi.grau@fje.edu>
2025-11-17 19:50:50 +0100

    Add files via upload

--- app/src/main/java/edu/fje/dam2/M24_AccesContactesActivity.java
+++ app/src/main/java/edu/fje/dam2/M24_AccesContactesActivity.java
@@ -1,5 +1,8 @@
-package edu.fje.dam2;
+package edu.fje.dam2.dam2pj6;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -12,7 +15,10 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 /**
  * Activitat que demostra el funcionament de l'accés als contactes del dispositiu
@@ -25,7 +31,18 @@ public class M24_AccesContactesActivity extends AppCompatActivity {
 
     private Button botoNom;
     static final int OBTENIR_CONTACTE = 1;
+    private static final int REQUEST_READ_CONTACTS_PERMISSION = 0;
+    private static final int REQUEST_CONTACT = 1;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if (requestCode == REQUEST_READ_CONTACTS_PERMISSION && grantResults.length > 0)
+        {
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,9 +58,11 @@ public class M24_AccesContactesActivity extends AppCompatActivity {
                 startActivityForResult(intent, OBTENIR_CONTACTE);
             }
         });
+        requestContactsPermission();
 
     }
 
+    @SuppressLint("Range")
     @Override
     public void onActivityResult(int reqCode, int resultCode, Intent data) {
         super.onActivityResult(reqCode, resultCode, data);
@@ -55,23 +74,17 @@ public class M24_AccesContactesActivity extends AppCompatActivity {
                     // crea el cursor amb le URI retornada
                     Cursor c = getContentResolver().query(contactData, null, null, null, null);
                     if (c.moveToFirst()) {
-                        String nom = c.getString(c.getColumnIndex(PhoneLookup.DISPLAY_NAME));
+                        @SuppressLint("Range") String nom = c.getString(c.getColumnIndex(PhoneLookup.DISPLAY_NAME));
 
-                        String nombre = c
-                                .getString(c
-                                        .getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
-                        String contactId = c.getString(c
-                                .getColumnIndex(ContactsContract.Contacts._ID));
+                        @SuppressLint("Range") String nombre = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                        @SuppressLint("Range") String contactId = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
                         if (nombre.equals("1")) {
                             Cursor telefons = getContentResolver()
                                     .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID
                                                     + " = " + contactId, null, null);
                             while (telefons.moveToNext()) {
-                                nombre = telefons
-                                        .getString(telefons
-                                                .getColumnIndex(ContactsContract.
-                                                        CommonDataKinds.Phone.NUMBER));
+                                nombre = telefons.getString(telefons.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                             }
                             telefons.close();
                         }
@@ -88,5 +101,17 @@ public class M24_AccesContactesActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.m24_acces_contactes, menu);
         return true;
     }
-
+    private boolean hasContactsPermission()
+    {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) ==
+                PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestContactsPermission()
+    {
+        if (!hasContactsPermission())
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_READ_CONTACTS_PERMISSION);
+        }
+    }
 }
